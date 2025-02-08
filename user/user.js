3f656c39e3da0bad89bb2f044428eb11ea248a49
muhammad-anas16 <anas.hanf38@gmail.com>
2025-02-09 00:57:14 +0500

    updated

--- user/user.js
+++ user/user.js
@@ -26,22 +26,37 @@ const check = () => {
     });
 }
 
-const myPost = async(theId) => {
+const myPost = async (theId) => {
     try {
 
-        const q =  query(collection(db, "product"), where("uid", "==", theId));
+        const q = query(collection(db, "product"), where("uid", "==", theId));
         const querySnapshot = await getDocs(q);
+        tBody.innerHTML = "";
         querySnapshot.forEach((product) => {
-            let showPost = document.createElement("div");     
-            console.log("My Post =>", product.data()); 
-            console.log("show =>", showPost);
-          });
+
+            let { name, Price } = product.data();
+            let tr = document.createElement("tr");
+            tr.classList = "border-b border-white";
+            tr.innerHTML = `
+            <th scope="row" class="px-6 py-4 font-medium whitespace-nowrap">
+                            ${name}"
+                        </th>
+                        <td class="px-6 py-4 border-r border-white">$${Price}</td>
+                        <td class="px-6 py-4 text-right">
+                            <a href="../Update_&_Delete/ud.html" class="font-medium text-blue-400 hover:underline">Edit</a>
+                        </td>
+            `;
+            tBody.appendChild(tr);
+
+        });
 
     } catch (err) {
         console.error(err);
     }
 }
 
+
+const tBody = document.getElementById("tBody");
 
 
 check();
